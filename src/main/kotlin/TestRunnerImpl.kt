51f22f270d017c2da3dc843f3f579023b5e24a1a
Oyuna <oyunaambaeva@gmail.com>
2023-11-27 00:44:13 +0300

    homework

---
+++ src/main/kotlin/TestRunnerImpl.kt
@@ -0,0 +1,17 @@
+import kotlin.reflect.full.declaredFunctions
+
+class TestRunnerImpl : TestRunner {
+    override fun <T> runTest(steps: T, test: () -> Unit) {
+        val functions = steps!!::class.declaredFunctions
+        val beforeMethods = functions.filter { it.name.startsWith("before") }
+        val afterMethods = functions.filter { it.name.startsWith("after") }
+
+        beforeMethods.forEach {
+            println("Run ${it.name}")
+        }
+        test()
+        afterMethods.forEach {
+            println("Run ${it.name}")
+        }
+    }
+}
